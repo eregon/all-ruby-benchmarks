7911a2b52ab744f435f0e87fdb77178feb0802f2
Chris Seaton <chris.seaton@oracle.com>
2016-07-23 17:53:55 +0100

    Add micro benchmarks for basic file IO.

---
+++ micro/core/file.rb
@@ -0,0 +1,44 @@
+# Copyright (c) 2016 Oracle and/or its affiliates. All rights reserved. This
+# code is released under a tri EPL/GPL/LGPL license. You can use it,
+# redistribute it and/or modify it under the terms of the:
+#
+# Eclipse Public License version 1.0
+# GNU General Public License version 2
+# GNU Lesser General Public License version 2.1
+
+benchmark 'core-open-close-file' do
+  f = File.open('/dev/null')
+  f.close()
+end
+
+null = File.open('/dev/null', 'w')
+
+kilobyte = 'x' * 1024
+
+if defined?(Truffle::Ropes.flatten_rope)
+  kilobyte = Truffle::Ropes.flatten_rope(kilobyte)
+end
+
+benchmark 'core-write-kilobyte' do
+  null.write kilobyte
+end
+
+gigabyte = 'x' * 1024 * 1024 * 1024
+
+if defined?(Truffle::Ropes.flatten_rope)
+  gigabyte = Truffle::Ropes.flatten_rope(gigabyte)
+end
+
+benchmark 'core-write-gigabyte' do
+  null.write gigabyte
+end
+
+zero = File.open('/dev/zero')
+
+benchmark 'core-read-kilobyte' do
+  zero.read 1024
+end
+
+benchmark 'core-read-gigabyte' do
+  zero.read 1024 * 1024 * 1024
+end
